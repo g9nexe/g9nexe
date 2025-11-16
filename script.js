951ee0fef52bd1d77f03eb1f6c9244a2d00e3939
g9nexe <hff1999@gmail.com>
2025-11-16 14:35:37 +0100

    add navigation and pages

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(() =>
 {
-	$("div.menu").load("menu.html");
+	$("div.menu").load("menu.html", () => $("[main-page]").click());
+});
+
+$(document).on("click", ".menu-item", function() {
+	let contentFileName = "page-content/" + $(this).data("content-file") + ".html";
+	$(".page-content").load(contentFileName);
 });
